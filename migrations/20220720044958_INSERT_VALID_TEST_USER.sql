37f9a2b038c9c2dae7336c45f992d731e6f3192e
Brett Buford <blbuford@gmail.com>
2022-07-26 22:31:38 -0700

    removed test data

--- migrations/20220720044958_INSERT_VALID_TEST_USER.sql
+++
@@ -1,3 +0,0 @@
--- Add migration script here
-INSERT INTO users (discord_user_id, discord_guild_id, goodreads_user_id, last_book_id)
-VALUES (562131151620145172, 996656225871740968, 2586157, '43848929');
